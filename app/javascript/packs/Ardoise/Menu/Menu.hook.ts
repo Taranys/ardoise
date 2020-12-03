453d289c5b1927895fd52eac0a52eb3b05a43adb
Yoann Prot <yoann.prot@gmail.com>
2020-12-03 16:56:46 +0100

    :sparkles: allow user to add simple category

--- app/javascript/packs/Ardoise/Menu/Menu.hook.ts
+++ app/javascript/packs/Ardoise/Menu/Menu.hook.ts
@@ -1,4 +1,5 @@
 import { useCallback, useEffect, useState } from 'react'
+import axios from 'axios'
 
 interface Category {
   id: string
@@ -13,21 +14,18 @@ export function useCategories() {
     (async () => {
       setLoading(true)
 
-      const response = await fetch('/api/categories')
-      // TODO manage error
-      setCategories(response.ok ? await response.json() : [])
+      const response = await axios.get<Category[]>('/api/categories')
+      setCategories(response.data)
 
       setLoading(false)
     })()
   }, [])
 
   const createCategory = useCallback(async () => {
-    const response = await fetch('/api/categories', { method: 'POST', body: JSON.stringify({ name: 'New category' }) })
-    // TODO manage error
-    setCategories(response.ok ? [...categories, await response.json()] : categories)
-  }, [])
+    const response = await axios.post<Category>('/api/categories', { name: 'New category' })
+    setCategories([...categories, response.data])
+  }, [categories])
 
-  console.log('🚀 ~ file: Menu.hook.ts ~ line 32 ~ useCategories ~ categories', categories)
   return {
     categories,
     createCategory,
